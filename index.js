5f0cf6c4ee909a54f9861b4a413a4165173820e5
Will <will@willacton.com>
2015-06-16 11:14:02 -0700

    reduce salt rounds because who cares?

--- index.js
+++ index.js
@@ -4,20 +4,13 @@ var bcrypt = require('bcrypt');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./ghost-dev.db', sqlite3.OPEN_READWRITE);
 
-bcrypt.genSalt(10, function(err, salt) {
+bcrypt.genSalt(4, function(err, salt) {
 	bcrypt.hash('password', salt, function(err, hash) {
-		console.log(hash);
-
 		db.serialize(function() {
-			// db.get("SELECT * FROM users WHERE id = 1", function(err, row) {
-			// 	console.log(row);
-			// });
-
 			db.run("UPDATE users SET password=? WHERE id = 1", hash);
 			
 			db.get("SELECT * FROM users WHERE id = 1", function(err, row) {
-				// console.log(row);
-				console.log("matches: "+bcrypt.compareSync("password", row.password));
+				if (bcrypt.compareSync("password", row.password)) console.log("Success!");
 			});
 		});
 
