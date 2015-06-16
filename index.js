63908b5c9b728197b163cee28d7fd9cb8b7e1ec6
Will <will@willacton.com>
2015-06-16 10:40:56 -0700

    ONLY TABS

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./ghost-dev.db');
 
 bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash('password', salt, function(err, hash) {
-    	console.log(hash);
+	bcrypt.hash('password', salt, function(err, hash) {
+		console.log(hash);
 
-    	db.serialize(function() {
-			db.get("SELECT * FROM users WHERE id = 1", function(err, row) {
-				console.log(row);
-			});
+		db.serialize(function() {
+			// db.get("SELECT * FROM users WHERE id = 1", function(err, row) {
+			// 	console.log(row);
+			// });
 
 			db.run("UPDATE users SET password=? WHERE id = 1", hash);
 			
@@ -18,10 +18,8 @@ bcrypt.genSalt(10, function(err, salt) {
 				// console.log(row);
 				console.log("matches: "+bcrypt.compareSync("password", row.password));
 			});
-
-
 		});
 
 		db.close();
-    });
+	});
 });
